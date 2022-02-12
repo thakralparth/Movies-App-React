6209d75163a4be86985b5cb9acf77e1427af3288
Parth Thakral <parth.thakral72@gmail.com>
2022-02-12 19:59:38 +0530

    Movies App Final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import Movies from './Components/Movies';
+import Favourite from './Components/Favourite';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';  ////provides access of Router to all its components inside
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+      {/* <Route path="/"  render={(props)=>(
+        <>
+          <Banner {...props}/>
+          <Movies {...props}/>
+        </>
+      )}/>   */}
+      <Route path="/" element={<><Banner /><Movies /></>}/>
+      <Route path="/favourites" element={<Favourite />}/>  
+      {/* <Banner />
+      <Movies /> */}
+      {/* <Favourite /> */}
+      </Routes>
+    </Router>
   );
 }
 
